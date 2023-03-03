228753e9787254c356c7f89bb2bc58c6106f82f9
SuburbanSaturn <eplopez.analyst@gmail.com>
2023-03-03 10:01:58 -0800

    new commit

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'restaurant'
+    'restaurant',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
