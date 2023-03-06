8fe8f9748f743a4744615f26ad4f32797577ab4b
SuburbanSaturn <eplopez.analyst@gmail.com>
2023-03-05 18:06:42 -0800

    adding views to booking and menu endpoints, routing urls in project-level urls file

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.index, name="home")
+    path('home/', views.index, name="home"),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('user/', views.userView.as_view()), 
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
+
+
+from .models import Booking, Menu
+from .serializers import bookingSerializer, menuSerializer, UserSerializer
 
 # Create your views here.
+
 def index(request):
     return render(request, 'index.html', {})
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = bookingSerializer
+    
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = menuSerializer    
+
+class userView(APIView):
+    
+    def get(self, request): 
+        items = User.objects.all()
+        serializer = UserSerializer(data = request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+    
